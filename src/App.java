00b6e8e83ab75de42a1fe87d70cab76222284a63
unknown <SALAS@EAN3076.universidad.ean>
2026-08-19 19:35:09 -0500

    Subiendo

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    //HOLA MUNDO
+    /*
+    CREE EN TI MISMO */
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/MiClase.java
@@ -0,0 +1,13 @@
+public class MiClase {
+ int Edad = 20;
+ double Altura = 1.72;
+ char Sexo = 'F' ;
+ boolean Aproved = true;
+ String nombre = "Sara Y Dieguis";
+ String saludo = "Hola, " + nombre;
+ double precio = 9.99;
+ int precioCortado = (int) precio;
+ int entero = 5;
+ double decimal = entero;
+
+}
